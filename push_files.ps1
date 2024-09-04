fd2c562b786684aa5ca1beab591f8f32e86ecdd8
TaeGum <tmznej12345678@gmail.com>
2024-09-05 00:28:10 +0900

    add .gitignore

--- push_files.ps1
+++ push_files.ps1
@@ -2,27 +2,23 @@ $FileDir = "C:\Side_Project"
 $Remote = "origin"
 $Branch = "master"
 
-
 Write-Output "Changing directory to $FileDir"
 Set-Location -Path "$FileDir" -ErrorAction Stop
-
 
 Write-Output "Getting list of all files"
 $allFiles = Get-ChildItem -Path "$FileDir" -ErrorAction Stop
 Write-Output "Found $($allFiles.Count) files."
-$allFiles | ForEach-Object { Write-Output $_.Name }
+$allFiles | ForEach-Object { Write-Output "$($_.Name)" }
 
-
-Write-Output "Filtering files with .7z in the name"
-$files = $allFiles | Where-Object { $_.Name -match "\.7z" }
-Write-Output "Found $($files.Count) files with .7z in the name."
-$files | ForEach-Object { Write-Output $_.Name }
-
+Write-Output "Filtering files with '7z' in the name"
+$files = $allFiles | Where-Object { $_.Name -match "7z" }
+Write-Output "Found $($files.Count) files with '7z' in the name."
+$files | ForEach-Object { Write-Output "$($_.Name)" }
 
 foreach ($file in $files) {
     Write-Output "Processing $($file.Name)"
     try {
-        git add $file.Name
+        git add "$($file.Name)"
         git commit -m "Add $($file.Name)"
         git push $Remote $Branch
     }
